58be316bf67880ea439481735127ca69652d5492
MicVick <mihir.hemnani99@gmail.com>
2022-06-30 23:36:13 +0530

    composer update 2

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit6c8b8d1a7576cbac356133a7c6af251d
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit6c8b8d1a7576cbac356133a7c6af251d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit6c8b8d1a7576cbac356133a7c6af251d', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e10e319ed87f3aff4535e2b2b70187ed9a4ee6b1',
+        'reference' => '3fa1cd0e14c23078eb5ec185b02aafc4b4b22848',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e10e319ed87f3aff4535e2b2b70187ed9a4ee6b1',
+            'reference' => '3fa1cd0e14c23078eb5ec185b02aafc4b4b22848',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
