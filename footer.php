f85d8b09b6ba0371ba46a4b2c6e0f9c31abbcbd5
Hemnani Mihir Jitumal <95122803+MicVick@users.noreply.github.com>
2023-01-30 22:17:00 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
 <p>Copyright &copy; 2022 - AnyDay Tutors. All Rights Reserved. || Magically Created by
-      <a href=" https://www.thewizards.me" target="_blank">
+      <a href="#" target="">
         <img class="logo-bottom" src="./wiz.png">The&nbsp;Wizards
       </a>
 </p> 
